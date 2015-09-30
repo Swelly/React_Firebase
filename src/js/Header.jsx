2ffe9e3e11a0ae4f68007dd385ab1fb1e01a9015
Swelly <Surfswell17@gmail.com>
2015-09-30 09:30:01 -0400

    getting data input now after binding input change to state

--- src/js/Header.jsx
+++ src/js/Header.jsx
@@ -1,21 +1,34 @@
 import React from 'react';
 
 class Header extends React.Component {
+  state = {
+    text: ''
+  }
 
-  handleClick() {
+  handleClick () {
     // Send value of text input to firebase
-    console.log("I was clicked");
+    console.log("I was clicked!");
+  }
+
+  handleInputChange (event) {
+    this.setState({text: event.target.value});
   }
 
   render () {
     return (
       <div className="input-group">
-        <input type="text" ref="newItem" placeholder="New Item" className="form-control"/>
+        <input
+          value={this.state.text}
+          onChange={this.handleInputChange.bind(this)}
+          type="text"
+          ref="newItem"
+          placeholder="New Item"
+          className="form-control"/>
         <span className="input-group-btn">
           <button
-          onClick={this.handleClick}
-          className="btn btn-default"
-          type="button">
+            onClick={this.handleClick}
+            className="btn btn-default"
+            type="button">
             Add
           </button>
         </span>
