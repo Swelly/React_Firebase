9a0751a91d4e8ffab6b46c19ae1972645c43c872
Wesley Magness <W.Magness17@gmail.com>
2015-10-01 22:57:03 -0400

    list item component was unnecesary

--- src/js/List.jsx
+++ src/js/List.jsx
@@ -1,23 +1,41 @@
 import React from 'react';
-import ListItem from './ListItem.jsx';
+// import ListItem from './ListItem.jsx';
 
 class List extends React.Component {
 
-  renderList () {
+  renderList() {
     if (this.props.items && Object.keys(this.props.items).length === 0) {
       return <h4>There are no ToDos</h4>
     } else {
-      var listChildren = this.props.items.map(item => {
+      var listChildren = this.props.items.map((item, index) => {
         return (
-          <ListItem
-            key={item.key}
-            item={item.text}
-            className="list-group-item"
-          >
-          </ListItem>
+          <div className="input-group">
+              <span className="input-group-addon">
+                  <input
+                      type="checkbox"
+                      onChange={this.handleDoneChange}
+                  />
+              </span>
+              <input
+                  type="text"
+                  className="form-control"
+                  key={item.key}
+                  value={item.text}
+              />
+              <span className="input-group-btn">
+                  <button className="btn btn-default">
+                      Delete
+                  </button>
+              </span>
+          </div>
         )
+
       });
-      return listChildren
+      return (
+        <div className="col-sm-12">
+          {listChildren}
+        </div>
+      )
     }
   }
 
--- src/js/ListItem.jsx
+++ src/js/ListItem.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
+import Rebase from 're-base';
+
+const base = Rebase.createClass('https://shining-inferno-2199.firebaseio.com/');
 
 class ListItem extends React.Component {
-    render () {
+    state = {
+        text: this.props.item,
+        done: this.props.item.done
+    };
+
+    componentWillMount() {
+    }
+
+    handleDoneChange(e) {
+        this.setState({done: e.target.checked})
+    }
+
+    render() {
         return (
-            <li>
-                {this.props.item}
-            </li>
+            <div className="input-group">
+                <span className="input-group-addon">
+                    <input
+                        type="checkbox"
+                        checked={this.state.done}
+                        onChange={this.handleDoneChange}
+                    />
+                </span>
+                <input
+                    type="text"
+                    className="form-control"
+                    value={this.state.text}
+                />
+                <span className="input-group-btn">
+                    <button className="btn btn-default">
+                        Delete
+                    </button>
+                </span>
+            </div>
         )
     }
 }
